7bf1b3d9cbca0f6277ad44e4a479389d33d56beb
viveksingh-01 <viveksingh9301@gmail.com>
2022-04-05 23:42:52 +0530

    wip: update BookCheckout struct

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ type Book struct {
 
 type BookCheckout struct {
 	BookID string `json:"book_id"`
+	User string `json:"user"`
 	CheckoutDate string `json:"checkout_date"`
 	IsGenesis bool `json:"is_genesis"`
 }
